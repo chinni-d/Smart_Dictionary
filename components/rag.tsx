7cd8323c7b09fffb3d6904c6cd215dcb27331f99
chinni-d <darapureddymanikanta8@gmail.com>
2026-01-23 13:22:14 +0530

    chore: update @dmanikanta17/chat-ui to latest version

--- components/rag.tsx
+++ components/rag.tsx
@@ -3,5 +3,5 @@ import { ChatUI } from "@dmanikanta17/chat-ui";
 import "@dmanikanta17/chat-ui/styles.css";
 
 export function Rag() {
-  return <ChatUI endpoint="" />;
+  return <ChatUI endpoint=""/>;
 }
